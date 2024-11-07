87a4c23423ff7f062746070901d238817f699ebd
Riley M <rileyjmeyers@gmail.com>
2024-11-07 21:19:28 +0000

    UI Skeleton put in place

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,24 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
-
+from flask import Flask, redirect, render_template, request, url_for, jsonify
 app = Flask(__name__)
 
+#Main Page
 @app.route('/')
-def hello_world():
-    return 'RFP Transformer'
+def main_page():
+    return render_template('main_page.html')
+
+#Proposal Parsing
+@app.route('/proposal/parsing')
+def proposal_parsing():
+    return render_template('proposal_parsing_page.html')
+
+
+#RFP Parsing
+@app.route('/rfp/parsing')
+def rfp_parsing():
+    return render_template('rfp_parsing_page.html')
+
+#Proposal Writing
+@app.route('/proposal/writing')
+def proposal_writing():
+    return render_template('proposal_writing_page.html')
 
